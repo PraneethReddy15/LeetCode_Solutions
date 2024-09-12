6e80425212dee0f07876662761089a4b3d70a789
K SAI PRANEETH REDDY <127538019+PraneethReddy15@users.noreply.github.com>
2024-09-12 17:10:01 +0530

    Time: 0 ms (100%), Space: 40.3 MB (58.5%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) 
+    {
+        // int sum=0;
+        // for(int i=1;i<=num/2;i++)
+        // {
+        //     if(num%i==0) sum+=i;
+        // }
+        // return sum==num;
+        return(num==6 || num==28 || num==496 || num==8128 || num==33550336 );
+    }
+}
