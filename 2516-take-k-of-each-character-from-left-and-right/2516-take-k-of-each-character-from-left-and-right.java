9728dbab66141871d91d0681c0cd872d6099b82a
K SAI PRANEETH REDDY <127538019+PraneethReddy15@users.noreply.github.com>
2024-11-20 12:48:12 +0530

    Time: 17 ms (21.82%), Space: 45.2 MB (75.15%) - LeetHub

---
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int takeCharacters(String s, int k) {
+        int n = s.length();
+        int l = 0, r = 0;
+        int ans = n;
+        int a = 0, b = 0, c = 0;
+
+        // Count total occurrences of 'a', 'b', and 'c'
+        int totalA = 0, totalB = 0, totalC = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == 'a') totalA++;
+            else if (ch == 'b') totalB++;
+            else if (ch == 'c') totalC++;
+        }
+
+        // If there are not enough 'a', 'b', or 'c', return -1
+        if (totalA < k || totalB < k || totalC < k) {
+            return -1;
+        }
+
+        // Reset counters and start sliding window
+        while (r < n) {
+            if (s.charAt(r) == 'a') a++;
+            if (s.charAt(r) == 'b') b++;
+            if (s.charAt(r) == 'c') c++;
+            r++;
+
+            // Shrink window if it exceeds the limits
+            while (a > totalA - k || b > totalB - k || c > totalC - k) {
+                if (s.charAt(l) == 'a') a--;
+                if (s.charAt(l) == 'b') b--;
+                if (s.charAt(l) == 'c') c--;
+                l++;
+            }
+
+            // Update the answer with the minimum number of characters to remove
+            ans = Math.min(ans, n - (r - l));
+        }
+
+        return ans;
+    }
+}
